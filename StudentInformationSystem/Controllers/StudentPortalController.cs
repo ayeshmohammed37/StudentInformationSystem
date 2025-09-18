4031e9a9fdfa37ab3a6391ceedb3e39df256183a
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-09-19 00:08:24 +0300

    WorkingOnStudentControllerActions

--- StudentInformationSystem/Controllers/StudentPortalController.cs
+++ StudentInformationSystem/Controllers/StudentPortalController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentInformationSystem.Data;
+using StudentInformationSystem.Models.CourseGradeModel;
+using StudentInformationSystem.Models.DepartmentModel;
+using StudentInformationSystem.Models.StudentModel;
+using StudentInformationSystem.ViewModels;
 
 namespace StudentInformationSystem.Controllers
 {
@@ -11,6 +15,19 @@ namespace StudentInformationSystem.Controllers
         {
             var students = context.Students.ToList();
             return View(students);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Student student = context.Students.FirstOrDefault(s => s.ID == id);
+
+            Department department = context.Departments.FirstOrDefault(d => d.DepartmentID == id);
+
+            List<CourseGrade> courseGrades = context.CourseGrades.Where(cg => cg.StudentID == id).Include(course => course.Course).ToList();
+
+            StudentDetailsVM studentDetails = new StudentDetailsVM(student, department, courseGrades);
+
+            return View(studentDetails);
         }
     }
 }
---
+++ StudentInformationSystem/ViewModels/StudentDetailsVM.cs
@@ -0,0 +1,21 @@
+﻿using StudentInformationSystem.Models.CourseGradeModel;
+using StudentInformationSystem.Models.DepartmentModel;
+using StudentInformationSystem.Models.StudentModel;
+
+namespace StudentInformationSystem.ViewModels
+{
+    public class StudentDetailsVM
+    {
+        public StudentDetailsVM(Student student, Department department, List<CourseGrade> courseGrades)
+        {
+            Student = student;
+            Department = department;
+            CourseGrades = courseGrades;
+        }
+
+        public Student Student { get; set; }
+        public Department Department { get; set; }
+
+        public List<CourseGrade> CourseGrades { get; set; }
+    }
+}
